fcafb69bd9c50c3f6a60bfec2c2bf8e4603e1abf
oleg.dekhtiarenko <oleg.dekhtiarenko@wonga.com>
2015-08-13 15:46:59 +0300

    qqqqqq

---
+++ ConsoleProj/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JenkinsTestClassLibrary;
+
+namespace ConsoleProj
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var api = new Api();
+
+			api.SayHello("Ted");
+			Console.ReadLine();
+		}
+	}
+}
