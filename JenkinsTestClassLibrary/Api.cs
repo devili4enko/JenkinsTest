346055af5a682b2f478a7bf93d7d290eca9cb95e
oleg.dekhtiarenko <oleg.dekhtiarenko@wonga.com>
2015-08-13 12:40:04 +0300

    Second commit

---
+++ JenkinsTestClassLibrary/Api.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTestClassLibrary
+{
+	public class Api
+	{
+		private const string Greeting = "Hello {0}!!!";
+
+		public void SayHello(string name)
+		{
+			Console.WriteLine(Greeting, name);	
+		}
+	}
+}
